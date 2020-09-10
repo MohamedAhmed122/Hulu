5e0954ca4a23fb690a2874541f84115b8771290d
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-10 19:56:39 +0700

    Implemented the Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import Header from './Component/Header/header';
+import Navbar from './Component/Navbar/navbar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
