9cd6edb7e2a5cca20ab3420478aaf0c10b4a2c82
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-10 19:36:09 +0700

    Navbar is Done

---
+++ src/Component/Header/header.jsx
@@ -0,0 +1,55 @@
+import React, { Fragment } from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import SearchIcon from '@material-ui/icons/Search';
+import { Grid, Typography } from "@material-ui/core";
+
+import useStyles from './Styles.jsx'
+
+
+const Header = () => {
+    const classes = useStyles()
+
+return (
+    <Fragment>
+        <Grid className={classes.header} container justify="space-between" alignItems="center">
+            <Grid item className={classes.icons}>
+                <Grid container >
+                <Grid item className={classes.icons}>
+                <HomeIcon className={classes.icon} fontSize='large'/>
+                    <p className={classes.textMain}>Home</p>
+                </Grid>
+                <Grid item className={classes.icons}>
+                <FlashOnIcon className={classes.icon} fontSize='large' />
+                    <p className={classes.text}>Trending</p>    
+                </Grid>
+                <Grid className={classes.icons}>
+                <OndemandVideoIcon className={classes.icon} fontSize='large' />
+                    <p className={classes.text}>Verified</p>
+                </Grid>
+                <Grid className={classes.icons}>
+                <VideoLibraryIcon className={classes.icon} fontSize='large' />
+                    <p className={classes.text}>Collections</p>
+                </Grid>
+                <Grid className={classes.icons}>
+                <SearchIcon className={classes.icon} fontSize='large' />
+                    <p className={classes.text}>Search</p>
+                </Grid>
+                <Grid className={classes.icons}>
+                <PermIdentityIcon className={classes.icon} fontSize='large'  />
+                    <p className={classes.text}>Account</p>
+                </Grid>           
+                </Grid>
+            </Grid>
+            <Grid item>
+                <Typography variant='h2' className={classes.main}>Hulu</Typography>
+            </Grid>
+        </Grid>
+        
+    </Fragment>
+);
+};
+export default Header;
