d346c6cd5c879ca95fb25b75e60ca45b1abc3ab9
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-10 23:09:18 +0700

    Refoctoring the Code & Ready To Publish

--- src/Component/Header/header.jsx
+++ src/Component/Header/header.jsx
@@ -1,55 +1,46 @@
-import React, { Fragment } from "react";
+import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import SearchIcon from '@material-ui/icons/Search';
-import { Grid, Typography } from "@material-ui/core";
 
-import useStyles from './Styles.jsx'
-
+import './Styles.css'
 
 const Header = () => {
-    const classes = useStyles()
+
 
 return (
-    <Fragment>
-        <Grid className={classes.header} container justify="space-between" alignItems="center">
-            <Grid item className={classes.icons}>
-                <Grid container >
-                <Grid item className={classes.icons}>
-                <HomeIcon className={classes.icon} fontSize='large'/>
-                    <p className={classes.textMain}>Home</p>
-                </Grid>
-                <Grid item className={classes.icons}>
-                <FlashOnIcon className={classes.icon} fontSize='large' />
-                    <p className={classes.text}>Trending</p>    
-                </Grid>
-                <Grid className={classes.icons}>
-                <OndemandVideoIcon className={classes.icon} fontSize='large' />
-                    <p className={classes.text}>Verified</p>
-                </Grid>
-                <Grid className={classes.icons}>
-                <VideoLibraryIcon className={classes.icon} fontSize='large' />
-                    <p className={classes.text}>Collections</p>
-                </Grid>
-                <Grid className={classes.icons}>
-                <SearchIcon className={classes.icon} fontSize='large' />
-                    <p className={classes.text}>Search</p>
-                </Grid>
-                <Grid className={classes.icons}>
-                <PermIdentityIcon className={classes.icon} fontSize='large'  />
-                    <p className={classes.text}>Account</p>
-                </Grid>           
-                </Grid>
-            </Grid>
-            <Grid item>
-                <Typography variant='h2' className={classes.main}>hulu</Typography>
-            </Grid>
-        </Grid>
-        
-    </Fragment>
+    <section className='header'>
+        <div className='header-icons'>
+            <div className='container'>
+                <HomeIcon fontSize='large'/>
+                <p className='text'>Home</p>
+            </div>
+            <div className='container'>
+                <FlashOnIcon  fontSize='large'/>
+                <p className='text'>Trending</p>
+            </div>
+            <div className='container'>
+                <VideoLibraryIcon fontSize='large' />
+                <p className='text'>Verified</p>
+            </div>
+            <div className='container'>
+                <OndemandVideoIcon  fontSize='large'/>
+                <p className='text'>Collections</p>
+            </div>
+            <div className='container'>
+                <SearchIcon  fontSize='large'/>
+                <p className='text'>Search</p>
+            </div>
+            <div className='container'>
+                <PermIdentityIcon fontSize='large'/>
+                <p className='text'>Account</p>
+            </div>
+        </div>
+        <h1>hulu</h1>
+    </section>
 );
 };
 export default Header;
